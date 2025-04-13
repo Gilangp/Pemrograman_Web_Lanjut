a1478c7ff6796efe3dd5ab92e2d958a6cf177951
Gilangp <gilangpurnomo0505@gmail.com>
2025-04-13 21:59:37 +0700

    update Tampilan Dashboard

--- PWL_POS/app/Http/Controllers/WelcomeController.php
+++ PWL_POS/app/Http/Controllers/WelcomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PenjualanModel;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
@@ -10,11 +12,34 @@ class WelcomeController extends Controller
     {
         $breadcrumb = (object) [
             'title' => 'Selamat Datang',
-            'list' => ['Home', 'Welcome']
+            'list' => ['Home', 'Dashboard']
         ];
 
         $activeMenu = 'dashboard';
 
-        return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+        // total omset penjualan
+        $omsetPenjualan = PenjualanModel::with('penjualanDetail')
+            ->get()
+            ->sum(function ($penjualan) {
+                return $penjualan->penjualanDetail->sum(function ($detail) {
+                    return $detail->jumlah * $detail->harga;
+                });
+            });
+
+        // top 3 barang terlaris
+        $topBarang = PenjualanModel::join('t_penjualan_detail', 't_penjualan.penjualan_id', '=', 't_penjualan_detail.penjualan_id')
+            ->join('m_barang', 't_penjualan_detail.barang_id', '=', 'm_barang.barang_id')
+            ->select('m_barang.barang_nama', DB::raw('sum(t_penjualan_detail.jumlah) as total_terjual'))
+            ->groupBy('m_barang.barang_nama')
+            ->orderByDesc('total_terjual')
+            ->take(3)
+            ->get();
+
+        return view('welcome', [
+            'breadcrumb' => $breadcrumb,
+            'activeMenu' => $activeMenu,
+            'omsetPenjualan' => $omsetPenjualan,
+            'topBarang' => $topBarang
+        ]);
     }
 }
--- PWL_POS/resources/views/welcome.blade.php
+++ PWL_POS/resources/views/welcome.blade.php
@@ -2,14 +2,34 @@
 
 @section('content')
 
-<div class="card">
-    <div class="card-header">
-        <h3 class="card-title">Halo, apakabar!!!</h3>
-        <div class="card-tools"></div>
+<!-- Row untuk Omset Penjualan dan Top 3 Produk -->
+<div class="row">
+    <!-- Omset Penjualan -->
+    <div class="col-md-6 col-12">
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title" style="font-size: 1.5rem;">Omset Penjualan</h3>
+            </div>
+            <div class="card-body text-center">
+                <p class="display-4" style="font-size: 2rem;">Rp {{ number_format($omsetPenjualan, 0, ',', '.') }}</p>
+            </div>
+        </div>
     </div>
-    <div class="card-body">
-        Selamat datang semua, ini adalah halaman utama dari aplikasi ini.
+
+    <!-- Top 3 Penjualan Terlaris -->
+    <div class="col-md-6 col-12">
+        <div class="card card-info">
+            <div class="card-header">
+                <h3 class="card-title" style="font-size: 1.5rem;">Top 3 Penjualan Terlaris</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-unstyled" style="font-size: 1rem;">
+                    @foreach ($topBarang as $barang)
+                        <li><span class="badge bg-info"></span> {{ $barang->barang_nama }} - {{ $barang->total_terjual }} Terjual</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
     </div>
 </div>
-
 @endsection
