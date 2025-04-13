c9c1b8fac51c1c1cf88fbc3273e0e0b592c63abb
Gilangp <gilangpurnomo0505@gmail.com>
2025-04-14 01:14:40 +0700

    update Akses sidebar

--- PWL_POS/resources/views/layouts/sidebar.blade.php
+++ PWL_POS/resources/views/layouts/sidebar.blade.php
@@ -19,7 +19,7 @@
                     <p>Dashboard</p>
                 </a>
             </li>
-            <li class="nav-header">Data Pengguna</li>
+            {{-- <li class="nav-header">Data Pengguna</li>
             <li class="nav-item">
                 <a href="{{ url('/level') }}" class="nav-link {{ ($activeMenu == 'level')? 'active' : '' }} ">
                     <i class="nav-icon fas fa-layer-group"></i>
@@ -64,7 +64,80 @@
                     <i class="nav-icon fas fa-cash-register"></i>
                     <p>Transaksi Penjualan</p>
                 </a>
+            </li> --}}
+            {{-- Untuk ADM --}}
+        @if(in_array(Auth::user()->level->level_kode, ['ADM', 'MNG']))
+            <li class="nav-header">Data Pengguna</li>
+        @endif
+        @if(Auth::user()->level->level_kode == 'ADM')
+            <li class="nav-item">
+                <a href="{{ url('/level') }}" class="nav-link {{ ($activeMenu == 'level') ? 'active' : '' }} ">
+                    <i class="nav-icon fas fa-layer-group"></i>
+                    <p>Level User</p>
+                </a>
             </li>
+        @endif
+
+        {{-- ADM, MNG --}}
+        @if(in_array(Auth::user()->level->level_kode, ['ADM', 'MNG']))
+            <li class="nav-item">
+                <a href="{{ url('/user') }}" class="nav-link {{ ($activeMenu == 'user') ? 'active' : '' }}">
+                    <i class="nav-icon far fa-user"></i>
+                    <p>Data User</p>
+                </a>
+            </li>
+            <li class="nav-header">Data Barang</li>
+            <li class="nav-item">
+                <a href="{{ url('/kategori') }}" class="nav-link {{ ($activeMenu == 'kategori') ? 'active' : '' }} ">
+                    <i class="nav-icon far fa-bookmark"></i>
+                    <p>Kategori Barang</p>
+                </a>
+            </li>
+        @endif
+
+        {{-- ADM, MNG, SUP --}}
+        @if(in_array(Auth::user()->level->level_kode, ['ADM', 'MNG']))
+            <li class="nav-item">
+                <a href="{{ url('/barang') }}" class="nav-link {{ ($activeMenu == 'barang') ? 'active' : '' }} ">
+                    <i class="nav-icon far fa-list-alt"></i>
+                    <p>Data Barang</p>
+                </a>
+            </li>
+        @endif
+
+        {{-- ADM, SUP --}}
+        @if(in_array(Auth::user()->level->level_kode, ['ADM','MNG', 'SUP']))
+            <li class="nav-header">Data Supplier</li>
+            <li class="nav-item">
+                <a href="{{ url('/supplier') }}" class="nav-link {{ ($activeMenu == 'supplier') ? 'active' : '' }} ">
+                    <i class="nav-icon fas fa-truck"></i>
+                    <p>Supplier</p>
+                </a>
+            </li>
+        @endif
+
+        @if(in_array(Auth::user()->level->level_kode, ['ADM', 'MNG', 'SUP', 'STF']))
+            <li class="nav-header">Data Transaksi</li>
+        @endif
+        {{-- ADM, MNG, SUP --}}
+        @if(in_array(Auth::user()->level->level_kode, ['ADM', 'MNG', 'SUP']))
+            <li class="nav-item">
+                <a href="{{ url('/stok') }}" class="nav-link {{ ($activeMenu == 'stok') ? 'active' : '' }} ">
+                    <i class="nav-icon fas fa-cubes"></i>
+                    <p>Stok Barang</p>
+                </a>
+            </li>
+        @endif
+
+        {{-- ADM, MNG, STF --}}
+        @if(in_array(Auth::user()->level->level_kode, ['ADM', 'MNG', 'STF']))
+            <li class="nav-item">
+                <a href="{{ url('/penjualan') }}" class="nav-link {{ ($activeMenu == 'penjualan') ? 'active' : '' }} ">
+                    <i class="nav-icon fas fa-cash-register"></i>
+                    <p>Transaksi Penjualan</p>
+                </a>
+            </li>
+        @endif
         </ul>
     </nav>
 </div>
